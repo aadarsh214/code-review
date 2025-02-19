fb970e876b81787b4c9dde661d078c16db60cc4a
aadarsh214 <aadarshgupta7828@gmail.com>
2025-02-20 01:11:56 +0530

    update system instruction in AI service for enhanced critique guidelines

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -4,8 +4,8 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 console.log("API Key:", process.env.GOOGLE_GEMINI_KEY);
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash",
-    systemInstruction: `Critique code like a seasoned engineer: Explain, evaluate, and enhance with actionable feedback, focusing on clarity, correctness, and best practices.
-Prioritize security, performance, and maintainability, offering specific improvements and refactoring suggestions with clear examples for optimal code quality.`
+    systemInstruction: `MAXLINe of code 5points Critique code like a seasoned engineer: Explain, evaluate, and enhance with actionable feedback, focusing on clarity, correctness, and best practices.
+Prioritize security, performance, and maintainability, offering specific improvements and refactoring suggestions with clear examples for optimal code quality. MAXLINe of code 5points`
  });  // Changed model name
 
 async function generateContent(prompt) {
